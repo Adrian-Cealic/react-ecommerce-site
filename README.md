94be80f4cee199d1357a494a7344619f954d17f9
Adrian-Cealic <adrian.cealic@gmail.com>
2024-07-23 22:41:38 +0300

    added price range timeout

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# React + Vite
+# React Fragrance App
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+-ReactJS (+Context API, hooks and routing)
+-TailwindCSS
+-Axios
+-My-Fragrance-Api (REST) (https://github.com/Adrian-Cealic/my-fragrance-api-v2)
+-and more
 
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
--- src/components/PriceRange.jsx
+++ src/components/PriceRange.jsx
@@ -1,18 +1,25 @@
 import React from 'react'
 
-const PriceRange = ({onChange}) => {
+const PriceRange = ({ onChange }) => {
 
+    let currentTimeout;
+    const onChangeHandler = (e) => {
+        clearTimeout(currentTimeout);
+        currentTimeout = setTimeout(() => {
+            onChange(e);
+        }, 500);
+    }
     return (
         <div className='bg-gray-200 py-4 px-6'>
             <p className='font-krona text-sm mb-2'>Select price</p>
             <div className="flex justify-between">
                 <div className="text-center">
-                    <input onChange={onChange} type="text" className='w-20 text-center p-2 focus:ring-accent focus:ring-inset caret-accent outline-none focus:ring-2 mb-2' placeholder='min' name='minPrice' />
+                    <input onChange={onChangeHandler} type="text" className='w-20 text-center p-2 focus:ring-accent focus:ring-inset caret-accent outline-none focus:ring-2 mb-2' placeholder='min' name='minPrice' />
                     <label className='text-sm' htmlFor="min">min</label>
                 </div>
                 <hr className='border border-accent w-full mt-4' />
                 <div className="text-center">
-                    <input onChange={onChange} type="text" className='w-20 text-center p-2 focus:ring-accent focus:ring-inset caret-accent outline-none focus:ring-2 mb-2' placeholder='max' name='maxPrice' />
+                    <input onChange={onChangeHandler} type="text" className='w-20 text-center p-2 focus:ring-accent focus:ring-inset caret-accent outline-none focus:ring-2 mb-2' placeholder='max' name='maxPrice' />
                     <label className='text-sm' htmlFor="max">max</label>
                 </div>
             </div>
